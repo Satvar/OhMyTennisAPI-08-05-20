6d12a7df3e8b83197ee349870abcf33cb196d0f2
Vadivel <vadivels@tech.cloudnausor.com>
2020-03-20 13:43:55 +0530

    [test]

--- app/modules/user/controller/user.js
+++ app/modules/user/controller/user.js
@@ -167,6 +167,43 @@ exports.registerUser = async function(req, res, next) {
   res.send(_output);
 };
 
+exports.user_set_table = async function(req, res, next) {
+  var _output = new output();
+  var arr = [
+    "animations",
+    "avaiablity",
+    "balance",
+    "bookingcourse_slot",
+    "booking_dbs",
+    "booking_slot_dbs",
+    "cities",
+    "coaches_dbs",
+    "courseclub_availablity",
+    "courseclub_year",
+    "course_collective_if_demand",
+    "course_dbs",
+    "couse_collective_if_club",
+    "email_template",
+    "individualcourses",
+    "payment_stripe",
+    "reserved_course",
+    "service",
+    "users",
+    "slot",
+    "tournament"
+  ];
+  for (let i = 0; i < arr.length; i++) {
+    await db_library.execute("DROP TABLE IF EXISTS " + arr[i], function(
+      err,
+      rows
+    ) {});
+  }
+  _output.data = {};
+  _output.isSuccess = true;
+  _output.message = "Le cours réussit";
+  res.send(_output);
+};
+
 exports.updateProfile = async function(req, res, next) {
   var _output = new output();
 
